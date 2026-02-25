45a87a4c5a0ea39c848150ed664cb858dd88afc0
czarintax <czarintax@gmail.com>
2026-02-25 20:18:49 +0100

    Revert "Fix message nesting issue in message forwarding"
    This reverts commit a0115aae264a764ea0b87710e68589c135b6d85d.

--- amxmodx/messages.cpp
+++ amxmodx/messages.cpp
@@ -349,13 +349,6 @@ void C_MessageEnd(void)
 						mres = mresB;
 		}
 		*/
-		
-		// Save message state before forwarding to prevent nesting issues
-		int saved_msgType = msgType;
-		int saved_msgDest = msgDest;
-		float *saved_msgOrigin = msgOrigin;
-		edict_t *saved_msgpEntity = msgpEntity;
-		
 		inhook = false;
 		if (mres & 1)
 		{
@@ -364,7 +357,7 @@ void C_MessageEnd(void)
 		}
 
 		/* send the real message */
-		MESSAGE_BEGIN(saved_msgDest, saved_msgType, saved_msgOrigin, saved_msgpEntity);
+		MESSAGE_BEGIN(msgDest, msgType, msgOrigin, msgpEntity);
 		Msg.Send();
 		MESSAGE_END();
 
